d85d7c28dfc6f418aba51c778037190e5054236e
zait <114537415+user10293-pixel@users.noreply.github.com>
2025-01-26 14:38:25 +0000

    v2

--- script.js
+++ script.js
@@ -3,4 +3,14 @@ function saludar(nombre) {
     console.log(`Hola, ${nombre}!`);
 }
 
+function despedir(nombre) {
+    console.log(`Adiós, ${nombre}!`);
+}
+
 saludar("Mundo");
+function despedir(nombre) {
+    console.log(`Adiós, ${nombre}!`);
+}
+
+saludar("Mundo");
+despedir("Mundo"); 
